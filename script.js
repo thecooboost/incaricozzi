0d17761a54920f801118229a74798d02d543ac9b
thecooboost <117953083+thecooboost@users.noreply.github.com>
2022-11-11 17:37:45 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -9,24 +9,20 @@ s4 = ('cervi')
 
 //Object that stores values of minimum and maximum angle for a value
 const rotationValues = [
-  { minDegree: 0, maxDegree: 30, value: 2 },
-  { minDegree: 31, maxDegree: 90, value: 1 },
-  { minDegree: 91, maxDegree: 150, value: 6 },
-  { minDegree: 151, maxDegree: 210, value: 5 },
-  { minDegree: 211, maxDegree: 270, value: 4 },
-  { minDegree: 271, maxDegree: 330, value: 3 },
-  { minDegree: 331, maxDegree: 360, value: 2 },
+  { minDegree: 0, maxDegree: 90, value: 1 },
+  { minDegree: 91, maxDegree: 180, value: 4 },
+  { minDegree: 181, maxDegree: 270, value: 3 },
+  { minDegree: 271, maxDegree: 360, value: 2 },
 ];
 //Size of each piece
-const data = [16, 16, 16, 16, 16, 16];
+const data = [16, 16, 16, 16];
 //background color for each piece
 var pieColors = [
   "#8b35bc",
   "#b163da",
   "#8b35bc",
   "#b163da",
-  "#8b35bc",
-  "#b163da",
+
 ];
 //Create chart
 let myChart = new Chart(wheel, {
